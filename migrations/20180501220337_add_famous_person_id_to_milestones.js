c87e3c63c9ad62b3ee023596b3fad7111a3e3835
spierre95 <stephen.pierre20@gmail.com>
2018-05-01 22:38:25 +0000

    used knex migration to create tables

---
+++ migrations/20180501220337_add_famous_person_id_to_milestones.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+return knex.schema.table('milestones',table=>{
+  table.integer('famous_person_id').references('famous_people')
+})
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('milestones',table=>{
+  table.dropColumn('famous_person_id')
+})
+};
